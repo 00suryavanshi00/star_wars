e0ec1b8ca75c18ef2ff88a09eec02e0dec85a878
Sanskar Suryavanshi <sanskarsuryavanshi@Sanskars-MacBook-Air.local>
2024-12-04 18:41:02 +0530

    fixed yodaloader

--- src/components/YodaLoader.tsx
+++ src/components/YodaLoader.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import babyyodaSrc from '../images/baby-yoda.svg';
 
 const YodaLoader = () => {
   return (
       <img
-        src="src/images/baby-yoda.svg"
+        src={babyyodaSrc}
         alt="Loading..."
         className="w-24 h-24 m-5 animate-bounce" // custom tailwind animation class
       />
