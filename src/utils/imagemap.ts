8605b66df4f8104b7e26c25f2d27bd42dd7ca5a3
Sanskar Suryavanshi <sanskarsuryavanshi@Sanskars-MacBook-Air.local>
2024-12-06 12:24:18 +0530

    changed to dynamic loading of images

--- src/utils/imagemap.ts
+++ src/utils/imagemap.ts
@@ -1,9 +1,21 @@
+
+import moviessrc from '../images/moviesbackground.jpg';
+import peoplesrc from '../images/peoplebackground.jpg';
+import planetssrc from '../images/planetsbackground.png';
+import speciessrc from '../images/speciesbackground.webp';
+import spaceshipssrc from '../images/spaceshipsbackground.webp';
+import vehiclessrc from '../images/vehiclesbackground.jpg'
+import mainbackground from '../images/nightbackground.jpg'
+
+
+
 export const resourceImageRoutes = new Map<string, string>([
-    ["films", "src/images/moviesbackground.jpg"],
-    ["people", "src/images/peoplebackground.jpg"],
-    ["planets", "src/images/planetsbackground.png"],
-    ["species", "src/images/speciesbackground.webp"],
-    ["starships", "src/images/spaceshipsbackground.webp"],
-    ["vehicles", "src/images/vehiclesbackground.jpg"],
+    ["films", moviessrc],
+    ["people", peoplesrc],
+    ["planets", planetssrc],
+    ["species", speciessrc],
+    ["starships", spaceshipssrc],
+    ["vehicles", vehiclessrc],
+    ["background", mainbackground]
   ]);
   
